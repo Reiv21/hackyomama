37f7b2b9226cadecff331d6cdb202e0a520fd29d
yakkudev <65334490+yakkudev@users.noreply.github.com>
2024-09-29 01:36:12 +0200

    Start camera at map centre

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour {
 
+    private void Start() {
+        float width = GridManager.instance.width;
+        float height = GridManager.instance.height;
+
+        Camera.main.transform.position = new Vector3((width / 2) - 0.5f, (height / 2) - 0.5f, -10);
+    }
+
     private void Update() {
         Vector3 pos = transform.position;
         float speed = 5;
@@ -26,6 +33,5 @@ public class CameraControls : MonoBehaviour {
         // Zoom in/out
         Camera.main.orthographicSize -= Camera.main.orthographicSize * Input.mouseScrollDelta.y * 0.1f;
         Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 1, 100);
-
     }
 }
