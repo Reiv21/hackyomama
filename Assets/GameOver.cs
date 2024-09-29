936b7887e9c9a5d975f9fa0185e5e737c7a93876
Justyn <lamor@poczta.onet.pl>
2024-09-29 08:40:38 +0200

    a

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -14,7 +14,7 @@ public class GameOver : MonoBehaviour {
 
     public int CalcualteScore() {
         int score = 0;
-        score += (LevelManager.instance.houseCount / LevelManager.instance.housesStart) * 100;
+        score += (LevelManager.instance.houseCount) * 100 /  LevelManager.instance.housesStart;
         score += (int) (BuildingManager.instance.money * 20f);
         return score;
     }
@@ -27,6 +27,9 @@ public class GameOver : MonoBehaviour {
     public void Win() {
         winPanel.SetActive(true);
         scoreText.text = "Score: " + CalcualteScore();
+        Leaderboard.instance.ShowLeaderboard();
+        Leaderboard.instance.AddPlayers(DateTime.Now.ToString("yyyy-MM-dd-HH:mm"), CalcualteScore() );
+        Leaderboard.instance.SavePlayerData();
     }
     public void Lost() {
         isGameOver = true;
@@ -35,6 +38,10 @@ public class GameOver : MonoBehaviour {
     public void TryAgain() {
         isGameOver = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void Next() {
+        isGameOver = false;
+        SceneManager.LoadScene(SceneManager.GetSceneByName(SceneManager.GetActiveScene().name).buildIndex + 1);
     }
     public void Menu() {
         SceneManager.LoadScene(0);
